85e50493b52c6bf4516ac0bb30be6c3da1780945
Msoda45 <msodayawari@gmail.com>
2026-01-03 08:49:59 +0430

    5.work

---
+++ 5.js/app.js
@@ -0,0 +1,12 @@
+function greating(){
+    console.log("Hello Everyone!")
+}
+greating();
+const meeting= function(name){
+    // console.log("Salam I'm Msoda");
+    return"I'm "+ name;
+    // console.log("Salam I'm Msoda");
+}
+const hiMsoda = meeting("M.soda Yawari");
+const hiNazanin = meeting("Nazanin Rasa");
+const hiTecher = meeting(" Student At Sharifi Programing Acadimy")
