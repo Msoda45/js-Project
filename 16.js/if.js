41c1f379a5625a39fdea56e8d4e9f2d183cd4aa5
Msoda45 <msodayawari@gmail.com>
2026-01-17 18:47:26 +0430

    16.js

---
+++ 16.js/if.js
@@ -0,0 +1,88 @@
+// const daraja = prompt("please enter your score");
+// if(daraja<=100 && daraja >=90){
+//     console.log("A");
+// }
+// else if(daraja>=80){
+//     console.log("B");
+// }
+// else if(daraja>=70){
+//     console.log("C");
+// }
+// else if(daraja>=60){
+//     console.log("D");
+// }
+// else{
+//     console.log("F")
+// }
+
+
+
+// const light = prompt("please enter your light");
+// if(light ===green){
+//     console.log("Go");
+// }
+// else if(light ===yellow){
+//     console.log("Slow Down");
+// }
+// else{
+//     console.log("Stop");
+// }
+
+
+// const dama = prompt("please enter your degree");
+// if(dama >= 35){
+//     console.log("HOT");
+// }
+// else if(dama<=34 && dama>=20){
+//     console.log("WARM");
+// }
+// else if(dama<=19 && dama >=10){
+//     console.log("NORMAL");
+// }
+// else{
+//     console.log("FREEZING")
+// }
+
+
+
+// const hads = prompt("یک عدد را حدس بزنید");
+// const number = Number(hads);
+// if(number <= 100 && number >=1){
+//     console.log("حدس تان درست است")
+// }
+// else{
+//     console.log("حدس تان اشتباه است")
+// }
+
+
+
+// const taqsim1 = 9;
+// const taqsim2 = 3;
+// if(taqsim1%taqsim2 ===0){
+//     console.log("عدد بخش پذیر است")
+// }
+// else{
+//     console.log("عدد بحش پذیر نیست")
+// }    
+
+
+// switch(true){
+//     case taqsim1%taqsim2 === 0:
+//         console.log(" عدد بخش پذیر است")
+//         break;
+//         default:
+//             console.log("عدد یخش پذیر نیست")
+//             break;
+// }
+
+
+
+
+
+
+
+
+
+           
+        
+
---
+++ 16.js/swich.js
@@ -0,0 +1,165 @@
+// const number = Number(prompt("enter a number of day"));
+// switch(number){
+//     case 1:
+//         console.log("Satrday");
+//         break;
+//         case 2:
+//             console.log("Sunday")
+//             break;
+//          case 3:
+//             console.log("Mondey");
+//             break;
+//             case 4:
+//                 console.log("Thusday")
+//                 break;
+//                 case 5:
+//                     console.log("Wendsday");
+//                     break;
+//                     case 6:
+//                         console.log("thurday")
+//                         break;
+//                      case 7:
+//                         console.log("Friday")
+// }
+
+
+
+// const mah = Number(prompt("plese enter your favorit month"));
+// switch(mah){
+//     case 1:
+//         console.log("Jan/ Monthe Have 31 Days");
+//         break;
+//         case 2:
+//             console.log("Feb/ Monthe Have 28 Days");
+//             break;
+//             case 3:
+//                 console.log("Mar/ Monthe Have 31 Days");
+//                 break;
+//                 case 4:
+//                      console.log("Apr/ Monthe Have 30 Days");
+//                 break;
+//                 case 5:
+//                      console.log("May/ Monthe Have 31 Days");
+//                 break;
+//                 case 6:
+//                      console.log("Jun/ Monthe Have 30 Days");
+//                 break;
+//                 case 7:
+//                      console.log("Jul/ Monthe Have 31 Days");
+//                 break;
+//                 case 8:
+//                      console.log("Aug/ Monthe Have 31 Days");
+//                 break;
+//                 case 9:
+//                      console.log("Sep/ Monthe Have 30 Days");
+//                 break;
+//                 case 10:
+//                      console.log("Oct/ Monthe Have 31 Days");
+//                 break;
+//                 case 11:
+//                      console.log("Nov/ Monthe Have 30 Days");
+//                 break;
+//                 case 12:
+//                      console.log("Des/ Monthe Have 31 Days");
+//                 break;   
+// }
+
+
+
+// const number1 = String(prompt("plase enter a day of score"));
+// switch (number1) {
+//    case "A".toLowerCase():
+//     console.log("Grade")
+//     break;
+//     case "B".toLowerCase():
+//         console.log("Good")
+//         break;
+//         case "C".toLowerCase():
+//             console.log("Medial")
+//             break;
+//             case "D".toLowerCase():
+//             console.log("Not bad")
+//             break;
+//             case "E".toLowerCase():
+//                 console.log("Bad")
+//                 break;
+//             case "F".toLowerCase():
+//                 console.log("Very Bad")
+//                 break;
+//     default:
+//         console.log("You Filed")
+//         break;
+// }
+
+
+
+
+// const furet = prompt("enter your favorite frute")
+// switch (furet){
+//      case "banana":
+//          console.log("Banana whit yellow color");
+//              break;
+//      case "apple":
+//          console.log("Apples whit green yellow & red color");
+//              break;
+//     case "annar":
+//          console.log("Annar whit red color");
+//               break;
+//  default:
+//           console.log("I don't know about this furte");
+//              break;
+// };
+
+
+
+
+
+// const codaCountry = Number(prompt("plese enter your number code"));
+// switch(codaCountry){
+//     case 93:
+//        console.log("this code from afghanistan");
+//        break;
+//     case 98:
+//     console.log("this code from Iran");
+//     break;
+//     case 63:
+//     console.log("This code form Asturillia");
+//     break;
+//      case 41:
+//     console.log("This code form Landan");
+//     break;
+
+// default:
+//     console.log("WE don't know you")
+// };
+
+
+
+
+
+// const math = prompt("لطفاَ کاراکتر مورد نظر خود را وارید نایید");
+// switch(math){
+//     case "*":
+//         console.log("این علامه ضرب است");
+//         break;
+//         case "/":
+//             console.log("این علامه تقسیم است");
+//             break;
+//             case "+":
+//             console.log("این علامه جم است");
+//             break;
+//                 case "-":
+//             console.log("این علامه منف است");
+//             break;
+//                 case "%":
+//             console.log("این علامه موژول است");
+//             break;
+//                 case "@":
+//             console.log("این علامه ات است");
+//             break;
+//                 case "#":
+//             console.log("این علامه هش است");
+//             break;
+//             default:
+//                 console.log("I don't know about it.")
+// }
