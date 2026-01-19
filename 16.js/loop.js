17a845bf9bd9ab81765bfd2f33ca2176c097a975
Msoda45 <msodayawari@gmail.com>
2026-01-19 18:34:15 +0430

    change.17

--- 16.js/loop.js
+++ 16.js/loop.js
@@ -4,9 +4,52 @@
 
 
 
-const numbers = [];
-numbers.push
-let totle = 1;
-for(let number1 =1 ; number1<; number1){
-console.log(number1)
-}
+// for(let number1 = 2 ; number1 <= 20; number1+=2){
+//     console.log(number1);
+// };
+
+
+// for(let number2 = 1;number2<=20;number2++){
+//     if(number2%2 ===0) {
+//         console.log(number2);
+//     }
+// }
+
+
+// let totle = 0;
+// for(let i = 1;i<=100;i++) {
+//     totle = totle+i;
+// }
+// console.log(totle);
+
+
+// const number3 = 7;
+// for(let i = 1;i<=10;i++) {
+//  console.log( i* number3)
+// }
+
+
+
+// for(let number4 = 20; number4 >=1; number4--){
+//     console.log(number4)
+// };
+
+
+
+// for(let number5 = 1; number5 <=50; number5++){
+//     if(number5% 5 ===0){
+//         console.log(number5)
+//     }
+// };
+
+
+
+
+// const zarb = (...a) =>{
+// let = totle = 1;
+//     for(let i = 0 ; i <8; i ++){
+//         totle = totle * a [i]
+//     }
+//     console.log(totle)
+// }
+// zarb(1,2,3,4,5,6,7,8)
