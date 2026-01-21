afa12ef6b9c57fa3963ed8d34f8eb77787a26e4f
Msoda45 <msodayawari@gmail.com>
2026-01-21 18:31:27 +0430

    change,3

---
+++ 16.js/do while.js
@@ -0,0 +1,18 @@
+// سوال اول
+// let i = 1 ;
+// while (i< 100) {
+//     console.log(i ** 2)
+//     i++
+// };
+
+
+
+// سوال دوم
+const list = [];
+while (true) {
+    const number = Number(prompt("Enter A Number"));
+    if(number === 0){
+        break;
+    }
+    list.push(number);
+;}
---
+++ 16.js/for.js
@@ -0,0 +1,159 @@
+// // سوال اول
+// for(let number = 1; i<=10 && i>=1; i++){
+//     console.log(i);
+// };
+
+
+// // سوال دوم
+// for(let number1 = 2 ; number1 <= 20; number1+=2){
+//     console.log(number1);
+// };
+
+// // سوال دوم
+// for(let number2 = 1;number2<=20;number2++){
+//     if(number2%2 ===0) {
+//         console.log(number2);
+//     }
+// }
+
+// // سوال سوم
+// let totle = 0;
+// for(let i = 1;i<=100;i++) {
+//     totle = totle+i;
+// }
+// console.log(totle);
+
+// // سوال چهارم
+// const number3 = 7;
+// for(let i = 1;i<=10;i++) {
+//  console.log( i+"* 7 =" + i * 7 )
+// }
+
+
+// // سوال پنجم
+// for(let number4 = 20; number4 >=1; number4--){
+//     console.log(number4)
+// };
+
+
+// // سوال ششم
+// for(let number5 = 1; number5 <=50; number5++){
+//     if(number5% 5 ===0){
+//         console.log(number5)
+//     }
+// };
+
+
+
+// // سوال هفتم
+// const zarb = (...a) =>{
+// let = totle = 1;
+//     for(let i = 0 ; i <8; i ++){
+//         totle = totle * a [i]
+//     }
+//     console.log(totle)
+// }
+// zarb(1,2,3,4,5,6,7,8)
+
+
+// // سوال هفتم
+// let number6 = 1;
+// for(let j = 1; j<=8;j++){
+//     number6 *=  j;
+// }
+// console.log(number6)
+
+
+// // سوال هشتم
+// let star = "";
+// for(let a = 1; a <=6; a++) {
+//     console.log("*".repeat(ass))
+// };
+
+
+
+// // سوال هفتم
+// for (let e = 1; e <= 6 ;e++){
+//     for(let f=0; f < e; f++){
+//         console.log("*");
+//     }
+//     console.log("")
+// }
+
+
+
+// // سوال نهم
+// for(let o = 6; o >=1; o--) {
+//     console.log("*".repeat(o))
+// };
+
+
+// سوال دهم
+// const number7 = 13;
+// let isprim = true;
+// if(number7 <=1) {
+//     isprim = false;
+// }
+// else{
+//     for(let r = 2 ; r < Math.sqrt(number7); r++){
+//         if(number7%r ===0){
+//             isprim = false;
+//             break;
+//         }
+//     }
+// }
+// if(isprim){
+//     console.log("The Number Is Prime");
+// }
+// else{
+//     console.log("The Number Is Not Prime");
+// };
+
+
+
+// سوال یازدهم
+// let totle1 = 0;
+// for (let k = 1 ; k <= 10 ; k ++){
+//     totle1 += k**2
+// }
+// console.log(totle1);
+
+
+
+// // سوال دوازدهم
+// let l = 0;
+// for(let p =5; p<=90; p++){
+//     if(p % 9 ===0){
+//        l += p; 
+//     }
+// };
+// console.log(l)
+
+
+
+// //سوال دوازدهم
+// let o = 0;
+// for(let p =5; p<=90; p++){
+//     if(p % 9 ===0){
+//        o = 1+o; 
+//     }
+// };
+// console.log(o);
+
+
+
+
+// //سوال سیزدهم
+// const arry = [15 , -19 , -3 , 0 , 8 , 12 , -7 , 4];
+// let postiv = 0;
+// let nagtive = 0;
+// for (let q = 0 ; q < arry.length; q ++){
+//     if(arry[q] >0){
+//         postiv = postiv +1;
+//     }
+//     else if(arry[q] <0 ){
+//         nagtive = nagtive +1;
+//     }
+// }
+// console.log("positive"  +"_"+postiv);
+// console.log( "nagtive"  +"_"+nagtive);
