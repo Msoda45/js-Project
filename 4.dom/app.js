43c8eff7906f836d16ef102950460a16ebe33fbb
Msoda45 <msodayawari@gmail.com>
2026-02-03 19:26:29 +0430

    change

---
+++ 4.dom/app.js
@@ -0,0 +1,9 @@
+const div = document.querySelector("div");
+const pragraf= document.querySelector("div> p:first-child");
+const div1 = document.querySelector("div>img:first-child");
+const section = document.querySelector("section");
+section.classList.add("con");
+div.classList.add("box2");
+div1.className="img";
+div.classList.add("box1");
+pragraf.className="p"
