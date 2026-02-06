7f523d2c84511f6570cd7e137dc4061000459aed
Msoda45 <msodayawari@gmail.com>
2026-02-06 18:34:16 +0430

    delade

--- 4.dom/app.js
+++
@@ -1,9 +0,0 @@
-const div = document.querySelector("div");
-const pragraf= document.querySelector("div> p:first-child");
-const div1 = document.querySelector("div>img:first-child");
-const section = document.querySelector("section");
-section.classList.add("con");
-div.classList.add("box2");
-div1.className="img";
-div.classList.add("box1");
-pragraf.className="p"
