3294b5316341b77d091d0a95e92c3ed3ea434dfd
Msoda45 <msodayawari@gmail.com>
2026-02-21 19:08:40 +0430

    dom.8

---
+++ 8.dom/1app.js
@@ -0,0 +1,56 @@
+// const number =prompt("please enter a number of day");
+// switch (number) {
+//     case "1":
+//         console.log("satrday")
+//         break;
+//     case "2":
+//         console.log("sunday")
+//         break;
+//         case "3":
+//             console.log("monday")
+//             break;
+//             case "4":
+//                 console.log("thusday")
+//                 break;
+//                 case "5":
+//                     console.log("wednsday")
+//                     break;
+//                     case "6":
+//                         console.log("thrsday")
+//                         break;
+//                         case "7":
+//                             console.log("friday")
+//                             break;
+//     default: 
+//     console.log("we don't know what day");
+//         break;
+// };
+const number1 = prompt("plase enter a day of score");
+switch (number1) {
+   case "A".toLowerCase():
+    console.log("Grade")
+    break;
+    case "B".toLowerCase():
+        console.log("Good")
+        break;
+        case "C".toLowerCase():
+            console.log("Medial")
+            break;
+            case "D".toLowerCase():
+            console.log("Not bad")
+            break;
+            case "E".toLowerCase():
+                console.log("Bad")
+                break;
+            case "F".toLowerCase():
+                console.log("Very Bad")
+                break;
+    default:
+        console.log("You Filed")
+        break;
+}
+// const frut = prompt("enter your favorite frute")
+// switch (frut){
+//                 case "*":
+//                     console.log("ضرب")
+//             }
---
+++ 8.dom/app.js
@@ -0,0 +1,24 @@
+// const lamp =prompt("please enter a lamp")
+// if(lamp === "red"){
+//     console.log("plase stop")
+// }
+// else if(lamp == "yellow"){
+//     console.log("please slow");
+// }
+// else if(lamp == "green"){
+//     console.log("you can go");
+// }
+// else{
+//     console.log("we dont know you")
+// }
+// const dama = prompt("please enter a number of dama");
+// if(dama >=35){
+    
+// }
+const number = prompt("enter a number")
+if(number >=0 && number<=100){
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
