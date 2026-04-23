cf9534a59b3711b4026142d1faf826d65063701b
Msoda45 <msodayawari@gmail.com>
2026-04-23 21:02:21 +0430

    changes

---
+++ app.js/19.js/app.js
@@ -0,0 +1,9 @@
+const name ="hamida yawari";
+function counter(name){
+    let count =0 ;
+    for(let i =0; i< name.length; i++){
+        count++;
+    }
+    return count;
+}
+    const result = counter("hamida yawari")
---
+++ app.js/23.js/app.js
@@ -0,0 +1,3 @@
+const message = "Hello Thear,I am Writing This Letter To Express My Interest In Your Seminar About Technolongy" ;
+const messageLength = message.length;
+console.log(messagelangth)
